b3df709b52c577784782f280d90149ba9a065b0f
tarun954 <tarunnaidu43637@gmail.com>
2021-08-24 21:47:49 +0530

    created preview cards for api data

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+import ActorGrid from '../Components/actor/ActorGrid';
 import Mainpage from '../Components/Mainpage'
+import ShowGrid from '../Components/show/ShowGrid';
 import { apiGet } from '../misc/config';
 
 const Home = () => {
@@ -34,10 +36,8 @@ const Home = () => {
 
     if (results && results.length > 0) {
       return results[0].show
-      ?results.map(item => <div key={item.show.id}>{item.show.name}</div>)
-         : results.map(item => (
-        <div key={item.person.id}>{item.person.name}</div>
-      ));
+      ?<ShowGrid data={results} />
+         : <ActorGrid data={results} />
          
         
     }
