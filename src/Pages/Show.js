f838e25140cc91906ae551473b4b598982371ab2
tarun954 <tarunnaidu43637@gmail.com>
2021-08-26 12:17:39 +0530

    created show pages and related data

--- src/Pages/Show.js
+++ src/Pages/Show.js
@@ -1,5 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import React, { useEffect, useReducer } from 'react';
 import { useParams } from 'react-router-dom';
+import Cast from '../Components/show/Cast';
+import Details from '../Components/show/Details';
+import Seasons from '../Components/show/Seasons';
+import ShowMainData from '../Components/show/ShowMainData';
 import { apiGet } from '../misc/config';
 
 const reducer = (prevState, action) => {
@@ -61,7 +66,27 @@ const Show = () => {
     return <div>Error occured: {error}</div>;
   }
 
-  return <div>this is show page</div>;
+  return (
+        <div>
+      <ShowMainData image={show.image} 
+      name={show.name} ratings={show.ratings} 
+  summary={show.summary} tags={show.genres} />
+      <div>
+          <h2>Details </h2>
+          <Details status={show.status}
+           network={show.network} 
+           premiered={show.premiered}  />
+      </div>
+      <div>
+          <h2>Seasons</h2>
+          <Seasons seasons={show._embedded.seasons} />
+      </div>
+      <div>
+          <h2>Cast </h2>
+          <Cast cast={show._embedded.cast} />
+      </div>
+  </div>
+  );
 };
 
 export default Show;
